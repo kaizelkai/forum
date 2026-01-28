3c7379269d36cc24c94affa62418f77ffce4f96b
kaizelkai <kabreemmanuel.ci@gmail.com>
2026-01-28 17:33:40 +0000

    chat5

--- page/composent/chat_sontent.php
+++ page/composent/chat_sontent.php
@@ -27,5 +27,45 @@
                 
             </div>
         </div>
+
+        <div class="chat">
+            <p>Salut Un unique serveur Apache permet de déployer simultanément plusieurs sites et services qu'on doit pour cela configurer individuellement. Pour plus de clarté, la ...</p>
+            <div class="infouser">
+                <img src="assets/1.png" alt="" srcset="">
+                <div class="userp">
+                    <h4>Kamao</h4>
+                    <p>25/12/2025 à 12h35</p>
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="chat">
+            <p>Salut Un unique serveur Apache permet de déployer simultanément plusieurs sites et services qu'on doit pour cela configurer individuellement. Pour plus de clarté, la ...</p>
+            <div class="infouser">
+                <img src="assets/1.png" alt="" srcset="">
+                <div class="userp">
+                    <h4>Kamao</h4>
+                    <p>25/12/2025 à 12h35</p>
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="chat">
+            <p>Salut Un unique serveur Apache permet de déployer simultanément plusieurs sites et services qu'on doit pour cela configurer individuellement. Pour plus de clarté, la ...</p>
+            <div class="infouser">
+                <img src="assets/1.png" alt="" srcset="">
+                <div class="userp">
+                    <h4>Kamao</h4>
+                    <p>25/12/2025 à 12h35</p>
+                </div>
+                
+            </div>
+        </div>
     </div>
+    <form action="#" method="post">
+        <textarea name="message" id="message" ></textarea>
+        <input type="submit" value="Publier" id="publier">
+    </form>
 </section>
