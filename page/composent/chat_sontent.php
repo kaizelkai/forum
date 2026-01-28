3e1367104e2b5ebfff10d3ac9808edc8fdab085c
kaizelkai <kabreemmanuel.ci@gmail.com>
2026-01-28 16:20:39 +0000

    chat 1

--- page/composent/chat_sontent.php
+++ page/composent/chat_sontent.php
@@ -1,3 +1,13 @@
 <section class="s2">
-    <center><h1>CHAT</h1></center>
+    <h2>TITRE</h2>
+    <div class="messagecontent">
+        <div class="chat">
+            <p>Salut Un unique serveur Apache permet de déployer simultanément plusieurs sites et services qu'on doit pour cela configurer individuellement. Pour plus de clarté, la ...</p>
+            <div class="infouser">
+                <img src="assets/1.png" alt="" srcset="">
+                <p>25/12/2025 à 12h35</p>
+            </div>
+        </div>
+        
+    </div>
 </section>
