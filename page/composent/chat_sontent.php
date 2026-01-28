2297b79105a6c170d5d879f3a4d80b98f3a4bdf6
kaizelkai <kabreemmanuel.ci@gmail.com>
2026-01-28 16:47:32 +0000

    chat 2

--- page/composent/chat_sontent.php
+++ page/composent/chat_sontent.php
@@ -5,9 +5,24 @@
             <p>Salut Un unique serveur Apache permet de déployer simultanément plusieurs sites et services qu'on doit pour cela configurer individuellement. Pour plus de clarté, la ...</p>
             <div class="infouser">
                 <img src="assets/1.png" alt="" srcset="">
-                <p>25/12/2025 à 12h35</p>
+                <div class="userp">
+                    <h4>Kamao</h4>
+                    <p>25/12/2025 à 12h35</p>
+                </div>
+                
             </div>
         </div>
         
+        <div class="chatuser">
+            <p>Salut Un unique serveur Apache permet de déployer simultanément plusieurs sites et services qu'on doit pour cela configurer individuellement. Pour plus de clarté, la ...</p>
+            <div class="infouser">
+                <img src="assets/1.png" alt="" srcset="">
+                <div class="userp">
+                    <h4>Kamao</h4>
+                    <p>25/12/2025 à 12h35</p>
+                </div>
+                
+            </div>
+        </div>
     </div>
 </section>
