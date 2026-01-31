84d2bbe5eae192a57b7e9201a067165e76fee9a2
emmanuel <kabreemmanuel.ci@gmail.com>
2026-01-31 16:06:00 +0000

    MJ

--- page/composent/datail_content.php
+++ page/composent/datail_content.php
@@ -1,13 +1,18 @@
 <section class="s2">
+    <?php
+        include_once __DIR__ . '/../../sgbd/dto/chatdto.php';
+        $title = $_GET['chat'] ?? 'Chat';
+        $chat=new ChatDTO();
+        $chatdata=$chat->readById($title);
+    ?>
     <div class="titreh">
-        <h2 class="titre">Une variable vide avec NULL</h2>
+        <h2 class="titre"><?php echo htmlspecialchars($chatdata->getTitle()); ?></h2>
     </div>
 
     <div class="detailcontent">
         <img class="detailImg" src="assets/B.jpeg" alt="" srcset="">
         <p class="detail">
-            Un navigateur web : il permet de tester la page web. Vous pouvez utiliser par exemple Mozilla Firefox, Internet Explorer, Google Chrome, Opera, Safari, ou tout autre navigateur auquel vous êtes habitués pour aller sur le web. Il est conseillé de tester son site régulièrement sur différents navigateurs.Un navigateur web : il permet de tester la page web. Vous pouvez utiliser par exemple Mozilla Firefox, Internet Explorer, Google Chrome, Opera, Safari, ou tout autre navigateur auquel vous êtes habitués pour aller sur le web. Il est conseillé de tester son site régulièrement sur différents navigateurs.
-            Un navigateur web : il permet de tester la page web. Vous pouvez utiliser par exemple Mozilla Firefox, Internet Explorer, Google Chrome, Opera, Safari, ou tout autre navigateur auquel vous êtes habitués pour aller sur le web. Il est conseillé de tester son site régulièrement sur différents navigateurs.Un navigateur web : il permet de tester la page web. Vous pouvez utiliser par exemple Mozilla Firefox, Internet Explorer, Google Chrome, Opera, Safari, ou tout autre navigateur auquel vous êtes habitués pour aller sur le web. Il est conseillé de tester son site régulièrement sur différents navigateurs
+            <?php echo htmlspecialchars($chatdata->getContent()); ?>
         </p>
     </div>
 </section>
