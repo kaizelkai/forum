15066adf7f4a6112674a1b12fc2e2c9fe546dd69
emmanuel <kabreemmanuel.ci@gmail.com>
2026-02-01 18:49:38 +0000

    auto git script bash MJ

--- gitmergerequest.sh
+++ gitmergerequest.sh
@@ -6,6 +6,10 @@ git checkout main
 #Mettre à jour
 git pull origin main
 
+#Fusionner les historiques si besoin
+#git merge master --allow-unrelated-histories
+
+
 #Merger la branche source
 git merge master
 
